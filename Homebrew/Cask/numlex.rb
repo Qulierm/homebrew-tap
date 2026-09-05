cdc5d6cb167d7109b39f56ebcca34521fb6e5c2d
Qulierm <nikita.gostevsky@gmail.com>
2026-09-05 21:25:11 +0300

    Add Numlex cask (4.5.1)

---
+++ Homebrew/Cask/numlex.rb
@@ -0,0 +1,19 @@
+cask "numlex" do
+  version "4.5.1"
+  desc "Natural-calculation notebook for the Mac"
+  homepage "https://github.com/Qulierm/Numlex"
+
+  url "https://github.com/Qulierm/Numlex/releases/download/4.5.1/Numlex-4.5.1-macOS-arm64.dmg"
+  sha256 "2d6a878030c64cbfd28e0597908191b47178103b71fcd5e7be0d3d5df63d4bf5"
+
+  name "Numlex"
+  desc "Natural-calculation notebook for the Mac"
+  desc "Type math, unit conversions, currency and weather in one plain-text sheet"
+
+  livecheck do
+    url "https://api.github.com/repos/Qulierm/Numlex/releases/latest"
+    regex /Numlex-(\d+(?:\.\d+)+)-macOS-arm64\.dmg/i
+  end
+
+  app "Numlex.app"
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Qulierm/homebrew-tap
+
+Custom Homebrew tap for [Numlex](https://github.com/Qulierm/Numlex).
+
+```
+brew tap Qulierm/tap
+brew install --cask Qulierm/tap/numlex
+```
