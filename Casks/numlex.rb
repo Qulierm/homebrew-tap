0e1323ca64bbbade241176b54ca77f6242c5afca
Nikita Gostevsky <nikita@Nikitas-MacBook-Air.local>
2026-09-13 21:08:30 +0300

    Update Numlex to 4.9.1

--- Casks/numlex.rb
+++ Casks/numlex.rb
@@ -1,8 +1,8 @@
 cask "numlex" do
-  version "4.9.0"
-  sha256 "c74e54db46574e41658a079504f9464bb01c4749fb60567440616d9c5475cd0f"
+  version "4.9.1"
+  sha256 "cbf3dd31518a30a6d3e46829b612b71ec1edc5d5129c62ebbdb4943f832c38c0"
 
-  url "https://github.com/Qulierm/Numlex/releases/download/4.9.0/Numlex-4.9.0-macOS-arm64.dmg"
+  url "https://github.com/Qulierm/Numlex/releases/download/4.9.1/Numlex-4.9.1-macOS-arm64.dmg"
   name "Numlex"
   desc "Notepad calculator with live math, units, currencies and dates"
   homepage "https://github.com/Qulierm/Numlex"
